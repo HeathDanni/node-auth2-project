2967a5f7a6e871da4df09da05c81f6130598fcc0
Heather Beckman <heatherbeckman85@gmail.com>
2021-01-16 10:15:57 -0700

    created users database

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-        filename: "./database/auth.db3",
+        filename: "./database/users.db3",
     },
     migrations: {
-        directory: "./datadase/migrations",
+        directory: "./database/migrations",
     },
     seeds: {
         directory: "./database/seeds",
